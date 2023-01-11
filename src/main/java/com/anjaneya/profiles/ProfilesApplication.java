52422664fc48a4d8d9561fdb3dd579814f049c3e
Arjan solanki <arjansolanki97@gmail.com>
2023-01-11 16:57:22 +0530

    Configure log using log4j2.xml
    by Arjan

--- src/main/java/com/anjaneya/profiles/ProfilesApplication.java
+++ src/main/java/com/anjaneya/profiles/ProfilesApplication.java
@@ -1,6 +1,8 @@
 package com.anjaneya.profiles;
 
 import com.anjaneya.profiles.cervice.WeatherService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfilesApplication implements CommandLineRunner {
+
+	private static final Logger logger = LogManager.getLogger(ProfilesApplication.class);
 
 	@Autowired
 	private WeatherService weatherService;
@@ -19,5 +23,11 @@ public class ProfilesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println(weatherService.forecast());
+
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Hey, This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 }
--- src/test/java/com/anjaneya/profiles/ProfilesApplicationTest.java
+++ src/test/java/com/anjaneya/profiles/ProfilesApplicationTest.java
@@ -1,10 +1,10 @@
 package com.anjaneya.profiles;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
@@ -13,7 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(OutputCaptureExtension.class)
 class ProfilesApplicationTest {
 
-    private Logger logger = LoggerFactory.getLogger(ProfilesApplicationTest.class);
+    //private Logger logger = LoggerFactory.getLogger(ProfilesApplicationTest.class);
+    private static final Logger logger = LogManager.getLogger(ProfilesApplicationTest.class);
+
 
     @Test
     public void testDefaultProfile(CapturedOutput output) {
